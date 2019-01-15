ce9507da8f99d4170bf99189134acda22d5b8295
Musthaq Ahamad <musthu.gm@gmail.com>
2019-01-15 11:37:23 +0530

    Rewamp movie grid ui

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,6 +1,6 @@
 module.exports = {
   siteMetadata: {
-    title: 'Geek Movies',
+    title: 'GeekTube',
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
--- src/components/movies_card/index.jsx
+++ src/components/movies_card/index.jsx
@@ -1,26 +1,23 @@
 import React from 'react';
 import Img from 'gatsby-image'
+import { Link } from 'gatsby'
 
 import './style.scss'
 
-function getStrippedContent( content) {
-    if (content.length > 150) {
-        return content.substring(0, 150)+". . ."
-    } else {
-        return content
-    }
+
+function titleCap(title) {
+    let splitTitle = title.split('')
+    splitTitle[0] = splitTitle[0].toUpperCase()
+    return splitTitle.join('')
 }
 const MoviesCard = ({ data, content }) => (
     <div class="movies-card-wrapper">
-        <div className="details-card">
-            <h2>{ data.title }</h2>
-            <h3>{ data.type }</h3>
-            <p>{ data.release }&nbsp;{ data.rating }</p>
-            <p dangerouslySetInnerHTML={{__html: getStrippedContent(content)}}></p>
-        </div>
         <div class="image-holder">
             <Img style={{height: '100%'}} fluid={data.poster.childImageSharp.fluid} />
+            <div className="rating">{ data.rating }</div>
         </div>
+        <p className="caption">{ data.release } • { titleCap(data.type) }</p>
+        <Link to={'/'+ data.title } className="title">{ data.title } </Link>
     </div>
 )
 
